66974d6ff5b9fe4a559cc910e243ad96b9cdc997
Sandbox user <tools@notanumber.com>
2026-01-17 00:13:13 +0000

    Please fix the following errors: Unable to resolve module ../../assets/icon.png from /home/user/workspace/src/app/welcome.tsx:   None of these files exist:   * icon.png   * assets/icon.png    6 |    7...

--- src/app/welcome.tsx
+++ src/app/welcome.tsx
@@ -5,7 +5,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { Image } from 'expo-image';
 
 // Logo image
-const LOGO_IMAGE = require('../../assets/icon.png');
+const LOGO_IMAGE = { uri: '/image-1768608072.png' };
 import {
   Users,
   ShoppingBag,
